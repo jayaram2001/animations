c27729e3168af9515abfcf7ba0a7d2129e9d91b9
jayaramasankaramoorthi <jayaramasankaramoorthi@gmail.com>
2024-08-26 16:46:19 +0000

    Implemented fade-in functionality.

--- src/fade.js
+++ src/fade.js
@@ -1,8 +1,16 @@
-export function fadeIn(a) {
-    console.log('hello' , a)
-    //fade In
+document.addEventListener('DOMContentLoaded', () => {
+let elements = document.getElementsByClassName('oands');
+fadeOut(elements[0] , 1000);
+function fadeOut(element , duration) {
+    element.style.opacity = 0;
+    intervel = duration/100;    
+    const intervalID =  setInterval(() => {
+        console.log(element.style.opacity)
+        element.style.opacity = parseFloat(element.style.opacity) + 0.01;
+        if(element.style.opacity >= 1){
+            clearInterval(intervalID)
+        }    
+        console.log(intervel);
+    },intervel)
 }
-
-function fadeOut() {
-    //fade Out
-}
+})

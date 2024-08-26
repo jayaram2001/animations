06045e51765a7efe41015f2b25ed0c9cc34075ef
francisvimal2002 <francisvimal2002@gmail.com>
2024-08-26 14:45:05 +0000

    Implemented Fade animation (fade.js)

--- src/fade.js
+++ src/fade.js
@@ -1,3 +1,22 @@
-function fadeIn() {
+const box = document.querySelector('.box');
+let duration = 3000;
+fadeIn(box, duration);
+function fadeIn(element, duration) {
     //fade In
+    console.log('inside');
+    let opacity = 0;
+    const increment = 1 / (duration / 16.66); // 16.66ms per frame for 60fps
+
+    function animate() {
+        opacity += increment;
+        element.style.opacity = opacity;
+
+        if (opacity < 1) {
+            requestAnimationFrame(animate);
+        } else {
+            element.style.opacity = 1;
+            // element.style.transform = "translateX(100%)"
+        }
+    }
+    animate();
 }
